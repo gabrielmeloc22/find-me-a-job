8ef53efea5ad4e79fae4c41a871d730604a11069
gabrielmeloc22 <gabrielpresb@gmail.com>
2024-09-27 09:13:47 -0300

    init

---
+++ crawler.py
@@ -0,0 +1,13 @@
+from crawl4ai import WebCrawler
+
+# Create an instance of WebCrawler
+crawler = WebCrawler()
+
+# Warm up the crawler (load necessary models)
+crawler.warmup()
+
+# Run the crawler on a URL
+result = crawler.run(url="https://www.nbcnews.com/business")
+
+# Print the extracted content
+print(result.markdown)
